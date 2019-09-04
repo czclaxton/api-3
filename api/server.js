0771e435ee7f23be0f977479cbd2d3a8a6139256
Connor Claxton <czclaxton@gmail.com>
2019-09-04 12:51:53 -0500

    update

---
+++ api/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const productsRouter = require("../products/products-router.js");
+
+const server = express();
+
+// global middleware
+server.use(express.json());
+
+server.use("/products", productsRouter);
+// server.use('/suppliers', suppliersRouter);
+
+// route handlers
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+// manage products (8)
+// manage clients (8)
+// manage orders
+// manage suppliers
+
+server.get("/clients", (req, res) => {});
+
+// export default server;
+module.exports = server;
